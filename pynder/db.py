95f5dc8f3d7dfe62838d0a53b033655ec6965d06
Michele Schimd <skimmoz@gmail.com>
2023-05-19 06:45:21 +0200

    Added license disclaimer and types.
    Started adding license disclaimer on top of source files and types to
    function parameters and return type. Mostly done for the 'pynder'
    module, need to start on the 'test' module.

--- pynder/db.py
+++ pynder/db.py
@@ -1,21 +1,37 @@
+# Copyright 2023 Michele Schimd
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==========================================================================
+"""Database management helper functions."""
 import sqlite3
 
-FILE_TABLE_NAME = "file"
-PUBLICATION_TABLE_NAME = "publication"
-TOPIC_TABLE_NAME = "topic"
-AUTHOR_TABLE_NAME = "author"
-
-def has_table(db, table):
+def has_table(db: sqlite3.Connection, table: str) -> bool:
     result = db.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table}';")
     table_exists = result.fetchone() is not None
     return table_exists
 
-def has_schema(cur):
+def has_schema(cur) -> bool:
     cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='file';")
     table_exists = cur.fetchone() is not None
     return table_exists
 
-def create_schema(db, script=None):
+def create_schema(db: sqlite3.Connection, script: str=None):
+    """_summary_
+
+    Args:
+        db (sqlite3.Connection): _description_
+        script (str, optional): _description_. Defaults to None.
+    """
     if has_schema(db.cursor()):
         return
     cur = db.cursor()
@@ -23,10 +39,10 @@ def create_schema(db, script=None):
         script = open("./sql/schema.sql").read()
     cur.executescript(script)
     
-def add_file(db, md5, path, publication=None):
+def add_file(db: sqlite3.Connection, md5: str, path: str, publication: int=None):
     pass
 
-def list_of_files(db):
+def list_of_files(db: sqlite3.Connection) -> list:
     result = db.execute(f"SELECT md5 FROM file;")
     return [row[0] for row in result]
 
--- pynder/fs.py
+++ pynder/fs.py
@@ -1,7 +1,22 @@
+# Copyright 2023 Michele Schimd
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==========================================================================
+
 import hashlib
 import os
 
-def hash_file(path, algorithm="md5"):
+def hash_file(path: str, algorithm: str="md5") -> str:
     if not os.path.isfile(path):
         raise IOError(f'File not found: ${path}')
     content = None
